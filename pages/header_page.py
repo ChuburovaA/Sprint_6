c70cb7d6d5e5402007179ff8b88f54f7629e2f96
Chuburova Alina <amminami@yandex.ru>
2024-04-22 18:52:38 +0300

    in file header_page.py add def click_yandex_logo

--- pages/header_page.py
+++ pages/header_page.py
@@ -4,3 +4,7 @@ from locators.header_page_locators import HeaderPageLocators
 from pages.base_page import BasePage
 
 class HeaderPage(BasePage):
+
+    @allure.step('Кликнуть на лого Яндекса')
+    def click_yandex_logo(self):
+        self.click_to_element(HeaderPageLocators.HEADER_PAGE_LOGO_YANDEX)
