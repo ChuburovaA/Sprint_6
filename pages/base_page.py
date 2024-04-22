b74b8fb97984204f271f3c408b48f0a3d32db093
Chuburova Alina <amminami@yandex.ru>
2024-04-22 18:41:50 +0300

    in file base_page.py add def find_element_with_wait

--- pages/base_page.py
+++ pages/base_page.py
@@ -7,3 +7,8 @@ class BasePage:
 
     def __init__(self, driver):
         self.driver = driver
+
+    @allure.step('Найти и убедиться, что элемент виден')
+    def find_element_with_wait(self, locator):
+        WebDriverWait(self.driver, 5).until(expected_conditions.visibility_of_element_located(locator))
+        return self.driver.find_element(*locator)
