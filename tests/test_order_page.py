d958d81881b5c91b826d90b4a074bce78a0ae1bb
Chuburova Alina <amminami@yandex.ru>
2024-04-22 19:38:15 +0300

    in file test_order_page.py add @pytest.mark.parametrize

--- tests/test_order_page.py
+++ tests/test_order_page.py
@@ -6,3 +6,14 @@ from conftest import driver, main_page, order_page
 
 @allure.feature('Заказ самоката на сайте "Яндекс Самокат"')
 class TestOrderPage:
+    @allure.story('Заполнение форм "Для кого самокат" и "Про аренду"')
+    @pytest.mark.parametrize("button, rent, checkbox, comment",
+                             [(OrderPageLocators.ORDER_BUTTON_UP,
+                               OrderPageLocators.ORDER_PAGE_RENT_TO_ONE_DAY,
+                               OrderPageLocators.ORDER_PAGE_BLACK_CHECKBOX,
+                               'Опаздаю!'),
+                              (OrderPageLocators.ORDER_BUTTON_DOWN,
+                               OrderPageLocators.ORDER_PAGE_RENT_TO_TWO_DAYS,
+                               OrderPageLocators.ORDER_PAGE_GREY_CHECKBOX,
+                               'Sorry, ill be 10min late')
+                              ])
