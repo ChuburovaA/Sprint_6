bd9a6a159ab008b8be9092659d102bf49ed1b910
Chuburova Alina <amminami@yandex.ru>
2024-04-22 19:04:02 +0300

    in file main_page.py add def get_url

--- pages/main_page.py
+++ pages/main_page.py
@@ -5,3 +5,7 @@ from locators.order_page_locators import OrderPageLocators
 from pages.base_page import BasePage
 
 class MainPage(BasePage):
+
+    @allure.step('Открыть страницу')
+    def get_url(self, url):
+        self.driver.get(url)
