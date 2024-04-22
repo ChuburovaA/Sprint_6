5159aac3194c48e876e656355c71690ae2670525
Chuburova Alina <amminami@yandex.ru>
2024-04-22 18:40:56 +0300

    in file base_page.py add new class BasePage

---
+++ pages/base_page.py
@@ -0,0 +1,9 @@
+import allure
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class BasePage:
+
+    def __init__(self, driver):
+        self.driver = driver
