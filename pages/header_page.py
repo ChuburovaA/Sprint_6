f64d074a7ee690e8a1a698f2cdaff08534103c9a
Chuburova Alina <amminami@yandex.ru>
2024-04-22 18:53:11 +0300

    in file header_page.py add def switch_to_dzen

--- pages/header_page.py
+++ pages/header_page.py
@@ -8,3 +8,7 @@ class HeaderPage(BasePage):
     @allure.step('Кликнуть на лого Яндекса')
     def click_yandex_logo(self):
         self.click_to_element(HeaderPageLocators.HEADER_PAGE_LOGO_YANDEX)
+
+    @allure.step('Дождаться на другой странице лого Дзена')
+    def switch_to_dzen(self):
+        self.wait_element(HeaderPageLocators.HEADER_PAGE_BUTTON_FIND)
