2894a09b01c80a321189b64d8daa892e304df4a2
Chuburova Alina <amminami@yandex.ru>
2024-04-22 19:01:23 +0300

    in file test_header_page.py add def test_logo_yandex

--- tests/test_header_page.py
+++ tests/test_header_page.py
@@ -12,3 +12,10 @@ class TestHeaderPage:
         main_page.click_button_cookie()
         header_page.click_logo_scooter()
         assert header_page.get_text()
+
+    @allure.story('Нажать на логотип "Яндекса" и в новом окне откроется главная страница "Яндекс Дзена"')
+    def test_logo_yandex(self, header_page, main_page):
+        main_page.click_button_cookie()
+        header_page.click_yandex_logo()
+        header_page.switch_to_dzen()
+        assert Urls.DZEN_URL in header_page.get_current_url()
