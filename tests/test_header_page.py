4752020d52113e126f95d6f87214f704abe0b27f
Chuburova Alina <amminami@yandex.ru>
2024-04-22 19:00:51 +0300

    in file test_header_page.py add def test_logo_scooter

--- tests/test_header_page.py
+++ tests/test_header_page.py
@@ -6,3 +6,9 @@ from conftest import driver, main_page, header_page
 
 @allure.feature('Проверка что происходит при нажатии на логотип "Яндекс Самокат"')
 class TestHeaderPage:
+
+    @allure.story('Нажать на логотип "Самокат", попадая при этом на главную странцу "Яндекс Самокат"')
+    def test_logo_scooter(self, main_page, header_page):
+        main_page.click_button_cookie()
+        header_page.click_logo_scooter()
+        assert header_page.get_text()
