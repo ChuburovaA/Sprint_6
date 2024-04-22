5e3145050f9bdac21dbf7c850162128ed4d55397
Chuburova Alina <amminami@yandex.ru>
2024-04-22 18:53:39 +0300

    in file header_page.py add def click_logo_scooter

--- pages/header_page.py
+++ pages/header_page.py
@@ -12,3 +12,7 @@ class HeaderPage(BasePage):
     @allure.step('Дождаться на другой странице лого Дзена')
     def switch_to_dzen(self):
         self.wait_element(HeaderPageLocators.HEADER_PAGE_BUTTON_FIND)
+
+    @allure.step('Кликнуть на лого Самоката')
+    def click_logo_scooter(self):
+        self.click_to_element(HeaderPageLocators.HEADER_PAGE_LOGO_SCOOTER)
