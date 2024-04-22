423b3f3dcb697692b31978e8613d2b930210365e
Chuburova Alina <amminami@yandex.ru>
2024-04-22 19:35:38 +0300

    in file test_main_page.py add @pytest.mark.parametrize

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -8,3 +8,16 @@ from conftest import driver, main_page
 
 @allure.feature('Сверка вопросов и ответов в списке "Вопросы о важном"')
 class TestMainPage:
+    @pytest.mark.parametrize(
+        "q_num,expected_result",
+        [
+            (0, Answers.ANSWER_0),
+            (1, Answers.ANSWER_1),
+            (2, Answers.ANSWER_2),
+            (3, Answers.ANSWER_3),
+            (4, Answers.ANSWER_4),
+            (5, Answers.ANSWER_5),
+            (6, Answers.ANSWER_6),
+            (7, Answers.ANSWER_7)
+        ]
+    )
