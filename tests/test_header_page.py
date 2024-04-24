69a883cd3e577129b255830933699d9d78597667
Chuburova Alina <amminami@yandex.ru>
2024-04-24 18:36:49 +0300

    remove @allure.story to @allure.title in file test_header_page.py

--- tests/test_header_page.py
+++ tests/test_header_page.py
@@ -7,13 +7,13 @@ from conftest import driver, main_page, header_page
 @allure.feature('Проверка что происходит при нажатии на логотип "Яндекс Самокат"')
 class TestHeaderPage:
 
-    @allure.story('Нажать на логотип "Самокат", попадая при этом на главную странцу "Яндекс Самокат"')
+    @allure.title('Нажать на логотип "Самокат", попадая при этом на главную странцу "Яндекс Самокат"')
     def test_logo_scooter(self, main_page, header_page):
         main_page.click_button_cookie()
         header_page.click_logo_scooter()
         assert header_page.get_text()
 
-    @allure.story('Нажать на логотип "Яндекса" и в новом окне откроется главная страница "Яндекс Дзена"')
+    @allure.title('Нажать на логотип "Яндекса" и в новом окне откроется главная страница "Яндекс Дзена"')
     def test_logo_yandex(self, header_page, main_page):
         main_page.click_button_cookie()
         header_page.click_yandex_logo()
